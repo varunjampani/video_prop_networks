533c29189e82ca7b39e306c5da147361a1333f60
Varun Jampani <varun.jampani@tue.mpg.de>
2017-04-19 15:54:46 +0200

    update in model links

--- data/get_seg_models.sh
+++ data/get_seg_models.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 curl -L https://www.dropbox.com/s/or3byhnrb58so67/deeplab_vpn_init_model.caffemodel?dl=0 > seg_models/deeplab_vpn_init_model.caffemodel
-curl -L https://www.dropbox.com/s/o50t1w0p4iidvym/SEG_FOLD0_STAGE1.caffemodel?dl=0 > seg_models/SEG_FOLD0_STAGE1.caffemodel
-curl -L https://www.dropbox.com/s/o50t1w0p4iidvym/SEG_FOLD1_STAGE1.caffemodel?dl=0 > seg_models/SEG_FOLD1_STAGE1.caffemodel
-curl -L https://www.dropbox.com/s/o50t1w0p4iidvym/SEG_FOLD2_STAGE1.caffemodel?dl=0 > seg_models/SEG_FOLD2_STAGE1.caffemodel
-curl -L https://www.dropbox.com/s/o50t1w0p4iidvym/SEG_FOLD3_STAGE1.caffemodel?dl=0 > seg_models/SEG_FOLD3_STAGE1.caffemodel
-curl -L https://www.dropbox.com/s/o50t1w0p4iidvym/SEG_FOLD4_STAGE1.caffemodel?dl=0 > seg_models/SEG_FOLD4_STAGE1.caffemodel
+curl -L https://www.dropbox.com/s/3netsxisfhqhui5/SEG_FOLD0_STAGE1.caffemodel?dl=0 > seg_models/SEG_FOLD0_STAGE1.caffemodel
+curl -L https://www.dropbox.com/s/mvhtyygz4hm01q2/SEG_FOLD1_STAGE1.caffemodel?dl=0 > seg_models/SEG_FOLD1_STAGE1.caffemodel
+curl -L https://www.dropbox.com/s/5uo6zqhmwfp5iwq/SEG_FOLD2_STAGE1.caffemodel?dl=0 > seg_models/SEG_FOLD2_STAGE1.caffemodel
+curl -L https://www.dropbox.com/s/g15t8g8drvt3a5t/SEG_FOLD3_STAGE1.caffemodel?dl=0 > seg_models/SEG_FOLD3_STAGE1.caffemodel
+curl -L https://www.dropbox.com/s/8srr5mpxll2e9m8/SEG_FOLD4_STAGE1.caffemodel?dl=0 > seg_models/SEG_FOLD4_STAGE1.caffemodel
